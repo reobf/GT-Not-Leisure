3658e301971513dbf30ef7449fef8a6620003fc7
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2024-12-26 15:07:05 +0800

    删除无用的检查

--- src/main/java/com/science/gtnl/common/machine/GenerationEarthEngine.java
+++ src/main/java/com/science/gtnl/common/machine/GenerationEarthEngine.java
@@ -196,16 +196,10 @@ public class GenerationEarthEngine extends GTPPMultiBlockBase<GenerationEarthEng
             true);
     }
 
-    public boolean checkHatches() {
-        return !mInputHatches.isEmpty() && !mInputBusses.isEmpty()
-            && !mOutputBusses.isEmpty()
-            && mOutputHatches.isEmpty();
-    }
-
     public boolean checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack) {
         tCountCasing = 0;
         if (!checkPiece(STRUCTURE_PIECE_MAIN, HORIZONTAL_OFF_SET, VERTICAL_OFF_SET, DEPTH_OFF_SET)) return false;
-        if (tCountCasing >= 5 && checkHatches()) {
+        if (tCountCasing >= 5) {
             updateHatchTexture();
             return true;
         }
