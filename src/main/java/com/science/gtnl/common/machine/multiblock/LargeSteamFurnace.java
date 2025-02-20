28953d224be8201c0a3d9fca992d52f70d4825e4
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-02-20 15:58:58 +0800

    整理代码

--- src/main/java/com/science/gtnl/common/machine/multiblock/LargeSteamFurnace.java
+++ src/main/java/com/science/gtnl/common/machine/multiblock/LargeSteamFurnace.java
@@ -10,7 +10,12 @@ import static gregtech.api.util.GTUtility.filterValidMTEs;
 import static gregtech.api.util.GTUtility.validMTEList;
 import static gtPlusPlus.core.block.ModBlocks.blockCustomMachineCasings;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 
